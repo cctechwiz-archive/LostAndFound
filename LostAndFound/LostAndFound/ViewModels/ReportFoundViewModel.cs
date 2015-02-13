d1463a9b9415885229d4c07a2c75be1262109901
Josh Maxwell <cctechwiz@gmail.com>
2015-02-12 17:14:57 -0700

    Adds stuff to the first tab (report a found)
    Adds raw XAML to the first tab.

---
+++ LostAndFound/LostAndFound/ViewModels/ReportFoundViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostAndFound.ViewModels
+{
+
+    class ReportFoundViewModel
+    {
+        private String _text = "lol hi dudes";
+
+        public String Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+    }
+}
