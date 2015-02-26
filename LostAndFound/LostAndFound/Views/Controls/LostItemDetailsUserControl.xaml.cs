8fb3f97b8d1ceb536f538dc1144dd7d12a794b05
BRYAN-SUU\Bryan <bryanwilbur@suumail.net>
2015-02-26 16:36:37 -0700

    ExpiredTagListUserControl view created and put in ExpiredItemsView.

--- LostAndFound/LostAndFound/Views/Controls/LostItemDetailsUserControl.xaml.cs
+++ LostAndFound/LostAndFound/Views/Controls/LostItemDetailsUserControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LostAndFound.Views
+namespace LostAndFound.Views.Controls
 {
     /// <summary>
     /// Interaction logic for LostItemDetailsUserControl.xaml
