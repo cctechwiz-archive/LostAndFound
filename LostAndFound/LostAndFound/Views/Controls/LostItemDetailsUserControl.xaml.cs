e803750a5864e9755f3e61a46925c146cf03361a
BRYAN-SUU\Bryan <bryanwilbur@suumail.net>
2015-02-26 16:06:14 -0700

    Revert "ExpiredTagListUserControl - Datagrid created and placed in View Expired Items view."
    This reverts commit 339017249d4d78b322c55f6dd5a91ae78ec8c200.

--- LostAndFound/LostAndFound/Views/Controls/LostItemDetailsUserControl.xaml.cs
+++ LostAndFound/LostAndFound/Views/Controls/LostItemDetailsUserControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LostAndFound.Views.Controls
+namespace LostAndFound.Views
 {
     /// <summary>
     /// Interaction logic for LostItemDetailsUserControl.xaml
