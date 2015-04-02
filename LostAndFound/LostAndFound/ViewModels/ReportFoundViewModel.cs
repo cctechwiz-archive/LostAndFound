9e47d935af5d83e984b9650530609f238d5b5d6d
Josh Maxwell <cctechwiz@gmail.com>
2015-04-02 14:35:00 -0600

    Gets the calling of the database working.
    This is scoped to a sheet level query right now and is populated by
    dummy data.

--- LostAndFound/LostAndFound/ViewModels/ReportFoundViewModel.cs
+++ LostAndFound/LostAndFound/ViewModels/ReportFoundViewModel.cs
@@ -1,20 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Controls;
 
 namespace LostAndFound.ViewModels
 {
 
     class ReportFoundViewModel
     {
-        private String _text = "lol hi dudes";
-
-        public String Text
+        public ReportFoundViewModel()
         {
-            get { return _text; }
-            set { _text = value; }
+            //var dtExcel = new DataTable();
+
+            //dtExcel.TableName = "MyExcelData";
+
+            //string SourceConstr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:\git\LostAndFound\LostAndFound\LostAndFound\Resources\LostAndFound.xlsx';Extended Properties= 'Excel 12.0 Xml;HDR=Yes;IMEX=1'";
+
+            //OleDbConnection con = new OleDbConnection(SourceConstr);
+            //string query = "Select * from [Sheet1$]";
+
+            //OleDbDataAdapter data = new OleDbDataAdapter(query, con);
+
+            //data.Fill(dtExcel);
+
+
+            //for (int i = 0; i < dtExcel.Rows.Count; i++)
+            //{
+            //    DataRow drow = dtExcel.Rows[i];
+
+            //    //Todo: Creat a model for the obejcts here and add them to the list so we can iterate over that list.
+            //}   
+
         }
+
+        public String Text { get; set; }
+
     }
 }
