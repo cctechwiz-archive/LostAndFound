378f00314693032a062a6cdb23b264b7b5c9f857
Josh Maxwell <cctechwiz@gmail.com>
2015-04-02 16:14:33 -0600

    Uncommented the code that accesses excel.

--- LostAndFound/LostAndFound/ViewModels/ReportFoundViewModel.cs
+++ LostAndFound/LostAndFound/ViewModels/ReportFoundViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Data;
 using System.Data.OleDb;
-using System.Windows.Controls;
 
 namespace LostAndFound.ViewModels
 {
@@ -10,26 +9,26 @@ namespace LostAndFound.ViewModels
     {
         public ReportFoundViewModel()
         {
-            //var dtExcel = new DataTable();
+            var dtExcel = new DataTable();
 
-            //dtExcel.TableName = "MyExcelData";
-            ////To get this to work you need to download and install the following: http://www.microsoft.com/en-us/download/details.aspx?id=13255
-            //string SourceConstr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:\git\LostAndFound\LostAndFound\LostAndFound\Resources\LostAndFound.xlsx';Extended Properties= 'Excel 12.0 Xml;HDR=Yes;IMEX=1'";
+            dtExcel.TableName = "MyExcelData";
+            //To get this to work you need to download and install the following: http://www.microsoft.com/en-us/download/details.aspx?id=13255
+            string SourceConstr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:\git\LostAndFound\LostAndFound\LostAndFound\Resources\LostAndFound.xlsx';Extended Properties= 'Excel 12.0 Xml;HDR=Yes;IMEX=1'";
 
-            //OleDbConnection con = new OleDbConnection(SourceConstr);
-            //string query = "Select * from [Sheet1$]";
+            OleDbConnection con = new OleDbConnection(SourceConstr);
+            string query = "Select * from [Sheet1$]";
 
-            //OleDbDataAdapter data = new OleDbDataAdapter(query, con);
+            OleDbDataAdapter data = new OleDbDataAdapter(query, con);
 
-            //data.Fill(dtExcel);
+            data.Fill(dtExcel);
 
 
-            //for (int i = 0; i < dtExcel.Rows.Count; i++)
-            //{
-            //    DataRow drow = dtExcel.Rows[i];
+            for (int i = 0; i < dtExcel.Rows.Count; i++)
+            {
+                DataRow drow = dtExcel.Rows[i];
 
-            //    //Todo: Creat a model for the obejcts here and add them to the list so we can iterate over that list.
-            //}   
+                //Todo: Creat a model for the obejcts here and add them to the list so we can iterate over that list.
+            }   
 
         }
 
