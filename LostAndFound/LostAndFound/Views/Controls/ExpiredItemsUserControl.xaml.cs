d05b8463ee0964cbf4a5a96ada2c9e6199cc6e00
bwilbur <bryanwilbur@suumail.net>
2015-04-27 00:25:24 -0700

    Expired Items View shows data

--- LostAndFound/LostAndFound/Views/Controls/ExpiredItemsUserControl.xaml.cs
+++ LostAndFound/LostAndFound/Views/Controls/ExpiredItemsUserControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using LostAndFound.Models;
+using LostAndFound.Services.Providers;
+using LostAndFound.Views.Windows;
+using LostAndFound.Views.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +24,64 @@ namespace LostAndFound.Views
     /// </summary>
     public partial class ExpiredItemsView : UserControl
     {
+        private int farBack;
+        private static List<FoundItem> _cachedFoundItems;
+        public static List<FoundItem> _foundItems;
+        public static bool reloadList;
+        private MyItem itemBasic;
+
         public ExpiredItemsView()
         {
+            farBack = -1;
+            reloadList = false;
+            FoundItemProvider _foundItemProvider = new FoundItemProvider();
+            _cachedFoundItems = _foundItemProvider.GetFoundItems();
+            _foundItems = new List<FoundItem>(_foundItemProvider.GetFoundItems());
             InitializeComponent();
+        }
+
+        public void selectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string text1 = (e.AddedItems[0] as ComboBoxItem).Content as string;
+            if (text1.Equals("7 days ago")) farBack = 7;
+            if (text1.Equals("30 days ago")) farBack = 30;
+            if (text1.Equals("60 days ago")) farBack = 60;
+            if (text1.Equals("90 days ago")) farBack = 90;
+            if (text1.Equals("The beginning of time")) farBack = 100000;
+            ExpiredItemListView.Items.Clear();
+            DateTime dateTime = DateTime.Now;
+            foreach (var item in _foundItems)
+            {
+                TimeSpan ts = dateTime - item.DateReported;
+                int td = ts.Days; 
+                if (td <= farBack)
+                {
+                    ExpiredItemListView.Items.Add(createMyItem(item));
+                }
+            }
+
+        }
+
+        private MyItem createMyItem(FoundItem item)
+        {
+            String tmp1 = "";
+            foreach (var desc in item.DescriptionTags)
+            {
+                tmp1 += desc.Name + " ";
+            }
+            String tmp2 = "";
+            foreach (var desc in item.LocationTags)
+            {
+                tmp2 += desc.Name + " ";
+            }
+            MyItem t;
+            t = new MyItem { desc = tmp1, loc = tmp2, date = item.DateReported.ToString("MM/dd/yyyy"), isVisible = true, isSelected = false };
+            return t;
+        }
+
+        private void itemClicked(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
